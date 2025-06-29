bc7c1fbfd87893e949b1f37cb0f64bdc731aa9b3
Satyam Kumar <sk4343@drexel.edu>
2025-06-29 18:47:44 -0400

    Initial spring boot project

---
+++ src/test/java/satyam/projects/reminder_app/ReminderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package satyam.projects.reminder_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
